3b96059cf7a24d30081ea549c60056cb5e945884
“wafflemakr” <wafflemakr@gmail.com>
2019-11-18 18:38:45 -0500

    add axios request to api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
+
+const sha256 = require("js-sha256").sha256;
 
 function App() {
+  const [response, setResponse] = useState(null);
+
+  useEffect(() => {
+    (async function send() {
+      const ts = Date.now() / 1000;
+      const verify = sha256(`10001|${ts}|gdm00vtvqhw4|`);
+
+      const data = {
+        clientID: 10001,
+        ts,
+        type: "json",
+        verify
+      };
+
+      try {
+        let response = await axios.post(
+          "https://cors-anywhere.herokuapp.com/https://harbor.medhub.com/functions/api/info/test",
+          data
+        );
+        setResponse(response);
+      } catch (e) {
+        alert(e);
+      }
+    })();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      {response && (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Response: {response.data.response}, Status: {response.status}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
     </div>
   );
 }
